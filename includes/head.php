0249c9f4c72ede5cefc4830bc0930fbe3c03fcf3
jurjenborkent <jurjenborkent@gmail.com>
2015-06-10 11:54:41 +0200

    Test commit

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="">
-    <meta name="author" content="Vrienden van">
+    <meta name="author" content="Vrienden Van">
     <link rel="shortcut icon" href="assets/ico/favicon.ico">
 
     <title>Adler</title>
